0870a858d2a1b09192d026abec6d6ac7dedd52cb
nicobascon <nico.bascon@live.fr>
2015-05-31 14:19:38 +0200

    Init appli

---
+++ src/ToDoListBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ToDoListBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('ToDoListBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
