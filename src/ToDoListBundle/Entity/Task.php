e2f501ce3441a849a38d241a972c595a511950ef
nicobascon <nico.bascon@live.fr>
2015-06-01 18:18:42 +0200

    Add entities from Doctrine

---
+++ src/ToDoListBundle/Entity/Task.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace ToDoListBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Task
+ *
+ * @ORM\Table(name="Task", indexes={@ORM\Index(name="taskslist_id", columns={"taskslist_id"})})
+ * @ORM\Entity
+ */
+class Task
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=false)
+     */
+    private $name;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="status", type="integer", nullable=false)
+     */
+    private $status;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var \ToDoListBundle\Entity\Taskslist
+     *
+     * @ORM\ManyToOne(targetEntity="ToDoListBundle\Entity\Taskslist")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="taskslist_id", referencedColumnName="id")
+     * })
+     */
+    private $taskslist;
+
+
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Task
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set status
+     *
+     * @param integer $status
+     * @return Task
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return integer 
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set taskslist
+     *
+     * @param \ToDoListBundle\Entity\Taskslist $taskslist
+     * @return Task
+     */
+    public function setTaskslist(\ToDoListBundle\Entity\Taskslist $taskslist = null)
+    {
+        $this->taskslist = $taskslist;
+
+        return $this;
+    }
+
+    /**
+     * Get taskslist
+     *
+     * @return \ToDoListBundle\Entity\Taskslist 
+     */
+    public function getTaskslist()
+    {
+        return $this->taskslist;
+    }
+}
---
+++ src/ToDoListBundle/Entity/Taskslist.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace ToDoListBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Taskslist
+ *
+ * @ORM\Table(name="TasksList")
+ * @ORM\Entity
+ */
+class Taskslist
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=false)
+     */
+    private $name;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="deadline", type="datetime", nullable=false)
+     */
+    private $deadline;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Taskslist
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set deadline
+     *
+     * @param \DateTime $deadline
+     * @return Taskslist
+     */
+    public function setDeadline($deadline)
+    {
+        $this->deadline = $deadline;
+
+        return $this;
+    }
+
+    /**
+     * Get deadline
+     *
+     * @return \DateTime 
+     */
+    public function getDeadline()
+    {
+        return $this->deadline;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
